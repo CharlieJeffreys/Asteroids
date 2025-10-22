dfeffc3a97fb2afa59cf7440611be3e28d2ac971
CharlieJeffreys <charlie.jeffreys5@gmail.com>
2025-10-22 09:22:22 +0300

    2: made a screen interface with a player object that can receive inputs (turning left and right)

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 import constants as con
+import player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((con.SCREEN_WIDTH, con.SCREEN_HEIGHT))
+    ship = player.Player(x = con.SCREEN_WIDTH/2, y = con.SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        ship.draw(screen)
+        ship.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
