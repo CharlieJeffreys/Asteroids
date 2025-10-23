28df07bcd268641619333e44ddb029d8ee627ff0
CharlieJeffreys <charlie.jeffreys5@gmail.com>
2025-10-23 13:43:54 +0300

    4: added asteroid field and collision dynamics to the screen, game exits when collision occurs.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import constants as con
 import player
+import asteroid
+import asteroidfield
 
 def main():
     pygame.init()
@@ -10,16 +12,27 @@ def main():
     screen = pygame.display.set_mode((con.SCREEN_WIDTH, con.SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable, )
+    field = asteroidfield.AsteroidField()
     player.Player.containers = (updatable, drawable)
     ship = player.Player(x = con.SCREEN_WIDTH/2, y = con.SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for obj in asteroids:
+            if obj.collision_check(ship):
+                print("Collision detected!")
+                raise SystemExit("Game over!")
+
         screen.fill("black")
         for drawing in drawable:
             drawing.draw(screen)
-            updatable.update(dt)
+            
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
